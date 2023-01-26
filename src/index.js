26a1a703b4c8aff8f991dc8b2049daeff11be6a6
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2023-01-26 06:49:03 +0300

    fix index

--- src/index.js
+++ src/index.js
@@ -19,7 +19,8 @@ const i = 3;// максимальное количество раундов
 // и если ответ верный - игра продолжается i раундов, иначе - конец игры
 export const check = (func, userName, correctAnswer) => {
   const userAns = userAnswer();// ввод и присваивание константе ответа игрока
-  if (correctAnswer == userAns) { // сравнение ответа игрока с правильным
+  const corAns = correctAnswer.toString();
+  if (corAns === userAns) { // сравнение ответа игрока с правильным
     console.log('Correct!');
     trueCounter += 1;// инкремент счетчика
     if (trueCounter < i) { // если кол-во выигранных раундов подряд < макс. кол-ва раундов
@@ -28,7 +29,7 @@ export const check = (func, userName, correctAnswer) => {
       console.log(`Congratulations, ${userName}!`);// поздравление игрока с победой
     }
   } else { // если ответ неверный - конец игры, "попробуй еще раз"
-    console.log(`'${userAns}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`'${userAns}' is wrong answer ;(. Correct answer was '${corAns}'.`);
     console.log(`Let's try again, ${userName}!`);
   }
 };
