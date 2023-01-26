b2deb59b13ae64fbbddded2f135af7c2df2ea68a
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2023-01-26 04:14:34 +0300

    ref brain-progression#2

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -6,20 +6,20 @@ import { check } from '../index.js';
 const userName = whatIsYourName();//ввод имени мользователя и приветствие
 console.log('What number is missing in the progression?');
 
+//создает прогрессию с пропущенным числом
 const makeProgression = (step = randomNumber()) =>{
-    let startNumber = randomNumber();
+    let startNumber = randomNumber();//начальное число
     let mas = [];
     for (let i = 0; i<10;i+=1){
         let nextNumber = startNumber += step;
-        mas.push(nextNumber);
+        mas.push(nextNumber);//заполняем массив
     }
-    let index = Math.floor(Math.random() * mas.length);
+    let index = Math.floor(Math.random() * mas.length);//рандомим индекс пропущенного числа
     let hiddenNumber = mas[index];
     //console.log(hiddenNumber);
-    mas[index] = '..';
-    //console.log(mas);
+    mas[index] = '..';//заменяем пропущенное число точками
     question(mas);
-    check(makeProgression, userName, hiddenNumber);
+    check(makeProgression, userName, hiddenNumber);//передача в функцию сравнения результата с ответом игрока 
 }
 
 export default makeProgression;
