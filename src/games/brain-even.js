addd6d473e5f6043b8ddfc57a371aa222c838d75
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2023-04-29 21:50:51 +0300

    this is the end?

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,12 +5,11 @@ import check from '../index.js';
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 // функция проверки числа не чётность/нечетность
-const isEven = (number) => (number % 2 === 0);
+const isEven = (number) => number % 2 === 0;
 
 const getAnswerAndQuestion = () => {
-  const number = randomNumber();
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-  const question = number;
+  const question = randomNumber();
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,13 +3,13 @@ import randomNumber from '../utils.js';
 
 // описание игры
 const description = 'What number is missing in the progression?';
+const progressionLength = 10;
 
 // функция для формирования прогрессии
 const getProgression = (startNumber, step) => {
   let nextNumber = startNumber;
   const mas = [];
-  const length = 10;
-  for (let i = 0; i < length; i += 1) {
+  for (let i = 0; i < progressionLength; i += 1) {
     nextNumber += step;
     mas.push(nextNumber);// заполняем массив
   }
@@ -17,7 +17,7 @@ const getProgression = (startNumber, step) => {
 };
 
 // создает прогрессию с пропущенным числом
-const makeProgression = () => {
+const getAnswerAndQuestion = () => {
   const startNumber = randomNumber(10);
   const step = randomNumber(10);
   const progression = getProgression(startNumber, step);// получаем сформированную прогрессию
@@ -26,11 +26,6 @@ const makeProgression = () => {
   const hiddenNumber = progression[hiddenNumberIndex];// получаем число
   progression[hiddenNumberIndex] = '..';// заменяем пропущенное число точками
   return [progression.join(' '), hiddenNumber];
-};
-
-const getAnswerAndQuestion = () => {
-  const [question, correctAnswer] = makeProgression();
-  return [question, correctAnswer];
 };
 
 check(getAnswerAndQuestion, description);
